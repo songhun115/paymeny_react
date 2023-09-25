db93ccfafa79d8e962fbac3d36f38e6beabbb535
songhun115 <hoho77113@daum.net>
2023-09-25 17:42:25 +0900

    react 파일생성 및세팅

---
+++ App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <button className="pay_btn">토스페이</button>
+    </div>
+  );
+}
+
+export default App;
